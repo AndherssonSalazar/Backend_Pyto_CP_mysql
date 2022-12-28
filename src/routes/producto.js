354ea5b49540c9f4f97f78fc3fa0c6306d719fe8
JONATAN PACORA VEGA <jona.jyn.27@hotmail.com>
2022-12-28 09:49:09 -0500

    producto terminado por 2

--- src/routes/producto.js
+++ src/routes/producto.js
@@ -9,9 +9,9 @@ router.get('/read/:codigo', productosCtrl.getProductoByCode);
 router.get('/stockminimo/', productosCtrl.getProductoByStockMinimo);
 router.get('/:_id', productosCtrl.getProductoById);
 
-router.post('/', productosCtrl.createProducto);
-router.put('/:_id', productosCtrl.updateProductById);
-router.put("/inhabilitar/:_id",  productosCtrl.updateProductInhabilitar);
-router.put("/habilitar/:_id", productosCtrl.updateProductHabilitar);
+router.post('/', [authJwt.verifyToken,authJwt.isJefeAlmacen],productosCtrl.createProducto);
+router.put('/:_id',[authJwt.verifyToken,authJwt.isJefeAlmacen], productosCtrl.updateProductById);
+router.put("/inhabilitar/:_id",[authJwt.verifyToken,authJwt.isJefeAlmacen],  productosCtrl.updateProductInhabilitar);
+router.put("/habilitar/:_id",[authJwt.verifyToken,authJwt.isJefeAlmacen], productosCtrl.updateProductHabilitar);
 
 module.exports= router;
