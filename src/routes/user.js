ea94a68c70144c2e5bda272f3afdb6aee61839c9
JONATAN PACORA VEGA <jona.jyn.27@hotmail.com>
2022-12-28 00:45:09 -0500

    modulo usuarios- falta update

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,7 +9,7 @@ router.get('/', [verifyToken, isAdmin],userCtrl.getUsers);
 router.get('/inhabilitados',[verifyToken, isAdmin], userCtrl.getUsersInhabiltados);
 router.get('/read/:dni', [verifyToken, isAdmin],userCtrl.getUserDni);
 router.post("/create",[verifyToken, isAdmin,checkExistingUser], userCtrl.createUser);
-router.put("/update/:_id", [verifyToken, isAdmin], userCtrl.updateUserById);
+// router.put("/update/:_id", [verifyToken, isAdmin], userCtrl.updateUserById);
 router.put("/inhabilitar/:_id", [verifyToken, isAdmin],userCtrl.updateUserInhabilitar);
 router.put("/habilitar/:_id",[verifyToken, isAdmin],  userCtrl.updateUserHabilitar);
 export default router;
