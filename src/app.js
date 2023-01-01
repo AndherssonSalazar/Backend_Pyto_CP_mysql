58880039671d2ca61a8a4b8425d03ceeda7d4033
JONATAN PACORA VEGA <jona.jyn.27@hotmail.com>
2023-01-01 15:18:54 -0500

    solicitud de endpoint de roles

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import categoriaRoutes from './routes/categoria'
 import auhtRoutes from './routes/auth'
 import usersRoutes from './routes/user'
 import movimientoRoutes from './routes/movimiento'
+import rolRoutes from './routes/roles'
 
 
 const cors = require('cors')
@@ -25,6 +26,7 @@ app.use('/api/categoria',categoriaRoutes)
 app.use('/api/auth',auhtRoutes)
 app.use('/api/users',usersRoutes)
 app.use('/api/movimiento',movimientoRoutes)
+app.use('/api/roles',rolRoutes)
 
 
 
